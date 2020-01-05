9f210fbd15d4f5501037221981b489e0b070be17
xiaosonggege <1243049371@qq.com>
2020-01-05 14:25:38 +0800

    commit

--- Environment.py
+++ Environment.py
@@ -13,8 +13,62 @@ import numpy as np
 class Map:
     """
     场景地图
-
+    :parameter
+    x_map: 地图长度
+    y_map: 地图宽度
+    client_num: 地图中client数量
+    MECserver_num: 地图中MECserver数量，默认MECserver均匀分布在地图中
+    R_client_range: client中cpu计算速率均值
+    R_MEC_range: MECserver中cpu计算速率均值
+    vxy_client_range: client移动速度分量范围
+    T_epsilon: 时间阈值
+    Q_client: client计算任务量阈值
+    Q_MEC: MECserver计算任务量阈值
+    r_edge_th: 服务范围边缘范围阈值
+    B: 无线信道带宽
+    N0: 高斯白噪声单边功率谱密度
+    P: 发射功率
+    h: 信道增益
+    delta: 发射功率随距离信源距离的衰减系数
     """
+    def __init__(self, x_map, y_map, client_num, MECserver_num, R_client_range, R_MEC_range,
+                 vxy_client_range, T_epsilon, Q_client, Q_MEC, r_edge_th, B, N0, P, h, delta):
+        """
+        场景地图构造函数
+        :param x_map: 地图长度
+        :param y_map: 地图宽度
+        :param client_num: 地图中client数量
+        :param MECserver_num: 地图中MECserver数量，默认MECserver均匀分布在地图中
+        :param R_client_range: client中cpu计算速率均值
+        :param R_MEC_range: MECserver中cpu计算速率均值
+        :param vxy_client_range: client移动速度分量范围
+        :param T_epsilon: 时间阈值
+        :param Q_client: client计算任务量阈值
+        :param Q_MEC: MECserver计算任务量阈值
+        :param r_edge_th: 服务范围边缘范围阈值
+        :param B: 无线信道带宽
+        :param N0: 高斯白噪声单边功率谱密度
+        :param P: 发射功率
+        :param h: 信道增益
+        :param delta: 发射功率随距离信源距离的衰减系数
+        """
+        rng = np.random.RandomState(0)
+        self.__x_map = x_map
+        self.__y_map = y_map
+        self.__client_num = client_num
+        self.__MECserver_num = MECserver_num
+        self.__R_client_range = R_client_range
+        self.__R_MEC_range = R_MEC_range
+        self.__vxy_client_range = vxy_client_range
+        self.__T_epsilon = T_epsilon
+        self.__Q_client = Q_client
+        self.__Q_MEC = Q_MEC
+        self.__r_edge_th = r_edge_th
+        self.__B = B
+        self.__N0 = N0
+        self.__P = P
+        self.__h = h
+        self.__delta = delta
 
 
 if __name__ == '__main__':
