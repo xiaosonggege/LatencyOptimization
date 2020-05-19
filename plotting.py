bc5e2fd886c84687b35022e1732f0180468d1e92
xiaosonggege <1243049371@qq.com>
2020-05-19 09:27:34 +0800

    commit

--- plotting.py
+++ plotting.py
@@ -13,10 +13,12 @@ import pandas as pd
 import re
 from matplotlib import pyplot as plt
 from scipy.interpolate import interp1d
+from matplotlib.ticker import FuncFormatter
 #插值
 spline = lambda x, y: interp1d(x, y, kind='quadratic')
 x_spline_new = np.linspace(1, 100, 1000)
-def plot1():
+kesi = lambda x, flag: np.where(x<0, x, 0) if flag==1 else np.where(x>0, x, 0)
+def plot1(regex = re.compile(pattern='-*\d+\.\d+')):
     """
     ddpg最好结果和slsqp对比
     :return:
@@ -29,28 +31,35 @@ def plot1():
                 break
             line_array = np.array([float(i) for i in line_str.split(' ')])
             ddpg = line_array if ddpg is None else np.vstack((ddpg, line_array))
+    ddpg_tanh = None
+    with open(file=r'/Users/songyunlong/Desktop/实验室/时延模型ddpg/ddpg_file/ddpg200-tanh.txt', mode='r') as f:
+        # sigmode
+        while True:
+            line_str = f.readline()
+            if not line_str:
+                break
+            generator = regex.finditer(string=line_str)
+            line_array = np.array([float(i.group(0)) for i in generator])
+            ddpg_tanh = line_array if ddpg_tanh is None else np.vstack((ddpg_tanh, line_array))
     with open(file=r'/Users/songyunlong/Desktop/实验室/时延模型ddpg/ddpg_file/slsqp.txt', mode='r') as f:
         line_str = f.readline()
         slsqp = np.array([float(i) for i in line_str.split(' ')])
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(ncols=1, nrows=2)
     x = [i for i in range(1, 101)]
-    ax.plot(x_spline_new, spline(x, ddpg[:, -1])(x_spline_new), c='r', label='ddpg')
-    ax.plot(x_spline_new, spline(x, slsqp)(x_spline_new), c='b', label='slsqp')
-    ax.legend(loc='upper right')
-    ax.set_xlabel('x/episode')
-    ax.set_ylabel('y/average latency')
-    ax.grid(axis='x', linestyle='-.')
-    ax.grid(axis='y', linestyle='-.')
-    fig.show()
+    ax[0].plot(x_spline_new, kesi(spline(x, ddpg[:, -1])(x_spline_new),2), c='r', label='ddpg')
+    ax[0].plot(x_spline_new, kesi(spline(x, slsqp)(x_spline_new),2), c='b', label='slsqp')
+    ax[0].legend(loc='upper right')
+    ax[0].set_xlabel('episode/time')
+    ax[0].set_ylabel('average latency/s')
+    ax[0].grid(axis='x', linestyle='-.')
+    ax[0].grid(axis='y', linestyle='-.')
 
-    fig, ax = plt.subplots()
-    x = [i for i in range(1, 101)]
-    ax.plot(x_spline_new, spline(x, ddpg[:, 0]*100)(x_spline_new), c='r', label='reward')
-    ax.legend()
-    ax.set_xlabel('x/eposide')
-    ax.set_ylabel('y/reward')
-    ax.grid(axis='x', linestyle='-.')
-    ax.grid(axis='y', linestyle='-.')
+    ax[1].plot(x_spline_new, kesi(x=spline(x, ddpg[:, -1]-slsqp)(x_spline_new), flag=2), label=r'$Df$')
+    ax[1].legend(loc='upper right')
+    ax[1].set_xlabel('episode/time')
+    ax[1].set_ylabel(r'$Df$')
+    ax[1].grid(axis='x', linestyle='-.')
+    ax[1].grid(axis='y', linestyle='-.')
     fig.show()
 
 def plot2(regex = re.compile(pattern='-*\d+\.\d+')):
@@ -91,29 +100,29 @@ def plot2(regex = re.compile(pattern='-*\d+\.\d+')):
             ddpg_lr3 = line_array if ddpg_lr3 is None else np.vstack((ddpg_lr3, line_array))
 
     #reward
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(ncols=1, nrows=2)
     x = range(1, 101)
-    ax.plot(x_spline_new, spline(x, ddpg_lr1[:, 0]*100)(x_spline_new), c='r', label='lr=1e-3')
-    ax.plot(x_spline_new, spline(x, ddpg_lr2[:, 0]*100)(x_spline_new), c='g', label='lr=1e-2')
-    ax.plot(x_spline_new, spline(x, ddpg_lr3[:, 0]*100)(x_spline_new), c='b', label='lr=5e-3')
-    ax.legend()
-    ax.set_xlabel('x/eposide')
-    ax.set_ylabel('y/reward')
-    ax.grid(axis='x', linestyle='-.')
-    ax.grid(axis='y', linestyle='-.')
-    fig.show()
+    ax[0].plot(x_spline_new, kesi(spline(x, ddpg_lr1[:, 0]*100)(x_spline_new),1), c='r', label='lr=1e-3')
+    ax[0].plot(x_spline_new, kesi(spline(x, ddpg_lr2[:, 0]*100)(x_spline_new),1), c='g', label='lr=1e-2')
+    ax[0].plot(x_spline_new, kesi(spline(x, ddpg_lr3[:, 0]*100)(x_spline_new),1), c='b', label='lr=5e-3')
+    ax[0].legend()
+    ax[0].set_xlabel('eposide/time')
+    ax[0].set_ylabel('reward')
+    ax[0].grid(axis='x', linestyle='-.')
+    ax[0].grid(axis='y', linestyle='-.')
+    # fig.show()
 
     #latency
-    fig, ax = plt.subplots()
+    # fig, ax = plt.subplots()
     x = range(1, 101)
-    ax.plot(x_spline_new, spline(x, ddpg_lr1[:, -1])(x_spline_new), c='r', label='lr=1e-3')
-    ax.plot(x_spline_new, spline(x, ddpg_lr2[:, -1])(x_spline_new), c='g', label='lr=1e-2')
-    ax.plot(x_spline_new, spline(x, ddpg_lr3[:, -1])(x_spline_new), c='b', label='lr=5e-3')
-    ax.legend()
-    ax.set_xlabel('x/eposide')
-    ax.set_ylabel('y/average latency')
-    ax.grid(axis='x', linestyle='-.')
-    ax.grid(axis='y', linestyle='-.')
+    ax[1].plot(x_spline_new, kesi(spline(x, ddpg_lr1[:, -1])(x_spline_new),2), c='r', label='lr=1e-3')
+    ax[1].plot(x_spline_new, kesi(spline(x, ddpg_lr2[:, -1])(x_spline_new),2), c='g', label='lr=1e-2')
+    ax[1].plot(x_spline_new, kesi(spline(x, ddpg_lr3[:, -1])(x_spline_new),2), c='b', label='lr=5e-3')
+    ax[1].legend()
+    ax[1].set_xlabel('eposide/time')
+    ax[1].set_ylabel('average latency/s')
+    ax[1].grid(axis='x', linestyle='-.')
+    ax[1].grid(axis='y', linestyle='-.')
     fig.show()
 
 def plot3(regex = re.compile(pattern='-*\d+\.\d+')):
@@ -154,29 +163,29 @@ def plot3(regex = re.compile(pattern='-*\d+\.\d+')):
             ddpg_batch3 = line_array if ddpg_batch3 is None else np.vstack((ddpg_batch3, line_array))
 
     # reward
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(ncols=1, nrows=2)
     x = range(1, 101)
-    ax.plot(x_spline_new, spline(x, ddpg_batch1[:, 0] * 100)(x_spline_new), c='r', label='batch size=1000')
-    ax.plot(x_spline_new, spline(x, ddpg_batch2[:, 0] * 100)(x_spline_new), c='g', label='batch size=500')
-    ax.plot(x_spline_new, spline(x, ddpg_batch3[:, 0] * 100)(x_spline_new), c='b', label='batch size=700')
-    ax.legend()
-    ax.set_xlabel('x/eposide')
-    ax.set_ylabel('y/reward')
-    ax.grid(axis='x', linestyle='-.')
-    ax.grid(axis='y', linestyle='-.')
-    fig.show()
+    ax[0].plot(x_spline_new, kesi(spline(x, ddpg_batch1[:, 0] * 100)(x_spline_new),1), c='r', label='batch size=1000')
+    ax[0].plot(x_spline_new, kesi(spline(x, ddpg_batch2[:, 0] * 100)(x_spline_new),1), c='g', label='batch size=500')
+    ax[0].plot(x_spline_new, kesi(spline(x, ddpg_batch3[:, 0] * 100)(x_spline_new),1), c='b', label='batch size=700')
+    ax[0].legend()
+    ax[0].set_xlabel('eposide/time')
+    ax[0].set_ylabel('reward')
+    ax[0].grid(axis='x', linestyle='-.')
+    ax[0].grid(axis='y', linestyle='-.')
+    # fig.show()
 
     # latency
-    fig, ax = plt.subplots()
+    # fig, ax = plt.subplots()
     x = range(1, 101)
-    ax.plot(x_spline_new, spline(x, ddpg_batch1[:, -1])(x_spline_new), c='r', label='batch size=1000')
-    ax.plot(x_spline_new, spline(x, ddpg_batch2[:, -1])(x_spline_new), c='g', label='batch size=500')
-    ax.plot(x_spline_new, spline(x, ddpg_batch3[:, -1])(x_spline_new), c='b', label='batch size=700')
-    ax.legend()
-    ax.set_xlabel('x/eposide')
-    ax.set_ylabel('y/average latency')
-    ax.grid(axis='x', linestyle='-.')
-    ax.grid(axis='y', linestyle='-.')
+    ax[1].plot(x_spline_new, kesi(spline(x, ddpg_batch1[:, -1])(x_spline_new),2), c='r', label='batch size=1000')
+    ax[1].plot(x_spline_new, kesi(spline(x, ddpg_batch2[:, -1])(x_spline_new),2), c='g', label='batch size=500')
+    ax[1].plot(x_spline_new, kesi(spline(x, ddpg_batch3[:, -1])(x_spline_new),2), c='b', label='batch size=700')
+    ax[1].legend()
+    ax[1].set_xlabel('eposide/time')
+    ax[1].set_ylabel('average latency/s')
+    ax[1].grid(axis='x', linestyle='-.')
+    ax[1].grid(axis='y', linestyle='-.')
     fig.show()
 
 
@@ -185,6 +194,9 @@ def plot4(regex = re.compile(pattern='-*\d+\.\d+')):
     reward对ddpg收敛性的影响
     :return:
     """
+    # with open(file=r'/Users/songyunlong/Desktop/实验室/时延模型ddpg/ddpg_file/slsqp.txt', mode='r') as f:
+    #     line_str = f.readline()
+    #     slsqp = np.array([float(i) for i in line_str.split(' ')])
     ddpg_ut = None
     with open(file=r'/Users/songyunlong/Desktop/实验室/时延模型ddpg/ddpg_file/ddpg200.txt', mode='r') as f:
         # ut
@@ -218,33 +230,34 @@ def plot4(regex = re.compile(pattern='-*\d+\.\d+')):
             ddpg_tanh = line_array if ddpg_tanh is None else np.vstack((ddpg_tanh, line_array))
 
     # reward
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(ncols=1, nrows=2)
     x = range(1, 101)
-    ax.plot(x_spline_new, spline(x, ddpg_ut[:, 0] * 100)(x_spline_new), c='r', label='ε(x)')
-    ax.plot(x_spline_new, spline(x, ddpg_sigmode[:, 0] * 100)(x_spline_new), c='g', label='sigmode(x)')
-    ax.plot(x_spline_new, spline(x, ddpg_tanh[:, 0] * 100)(x_spline_new), c='b', label='tanh(x)')
-    ax.legend()
-    ax.set_xlabel('x/eposide')
-    ax.set_ylabel('y/reward')
-    ax.grid(axis='x', linestyle='-.')
-    ax.grid(axis='y', linestyle='-.')
-    fig.show()
+    ax[0].plot(x_spline_new, kesi(spline(x, ddpg_ut[:, 0] * 100)(x_spline_new),1), c='r', label='ε(x)')
+    ax[0].plot(x_spline_new, kesi(spline(x, ddpg_sigmode[:, 0] * 100)(x_spline_new),1), c='g', label='sigmode(x)')
+    ax[0].plot(x_spline_new, kesi(spline(x, ddpg_tanh[:, 0] * 100)(x_spline_new),1), c='b', label='tanh(x)')
+    ax[0].legend()
+    ax[0].set_xlabel('eposide/time')
+    ax[0].set_ylabel('reward')
+    ax[0].grid(axis='x', linestyle='-.')
+    ax[0].grid(axis='y', linestyle='-.')
+    # fig.show()
 
     # latency
-    fig, ax = plt.subplots()
+    # fig, ax = plt.subplots()
     x = range(1, 101)
-    ax.plot(x_spline_new, spline(x, ddpg_ut[:, -1])(x_spline_new), c='r', label='ε(x)')
-    ax.plot(x_spline_new, spline(x, ddpg_sigmode[:, -1])(x_spline_new), c='g', label='sigmode(x)')
-    ax.plot(x_spline_new, spline(x, ddpg_tanh[:, -1])(x_spline_new), c='b', label='tanh(x)')
-    ax.legend()
-    ax.set_xlabel('x/eposide')
-    ax.set_ylabel('y/average latency')
-    ax.grid(axis='x', linestyle='-.')
-    ax.grid(axis='y', linestyle='-.')
+    # ax[1].plot(x_spline_new, kesi(spline(x, slsqp)(x_spline_new), 2), c='m', label='slsqp')
+    ax[1].plot(x_spline_new, kesi(spline(x, ddpg_ut[:, -1])(x_spline_new),2), c='r', label='ε(x)')
+    ax[1].plot(x_spline_new, kesi(spline(x, ddpg_sigmode[:, -1])(x_spline_new),2), c='g', label='sigmode(x)')
+    ax[1].plot(x_spline_new, kesi(spline(x, ddpg_tanh[:, -1])(x_spline_new),2), c='b', label='tanh(x)')
+    ax[1].legend()
+    ax[1].set_xlabel('eposide/time')
+    ax[1].set_ylabel('average latency/s')
+    ax[1].grid(axis='x', linestyle='-.')
+    ax[1].grid(axis='y', linestyle='-.')
     fig.show()
 
 if __name__ == '__main__':
-    # plot1()
+    plot1()
     # plot2()
     # plot3()
-    plot4()
+    # plot4()
